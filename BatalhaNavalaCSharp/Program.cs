90d5db4f275a44803e08123a25d0aa5c9dff0072
Analugoldacker <analugoldacker>
2023-08-30 15:26:55 -0300

    arquivo de texto explicação do jogo

--- BatalhaNavalaCSharp/Program.cs
+++ BatalhaNavalaCSharp/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             string[,] grade = new string[11, 11];
-            //grade[0, 0] = "x";                
+            grade[0, 0] = "x";                
             preencher(grade);
 
-            //vencedor();
-            //listarMatriz(grade);
+            // vencedor();
+            listarMatriz(grade);
 
 
             //jogar
